3d9c578368f7ce75de03dc65031bb5ca472ac9ad
unknown <jamesgarlandraney@gmail.com>
2025-02-05 00:04:08 -0700

    Updated Code to Project

--- Develop/client/src/utils/auth.ts
+++ Develop/client/src/utils/auth.ts
@@ -3,28 +3,46 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
-  }
+  const token = this.getToken();
+  const decoded = jwtDecode<JwtPayload>(token);
+  return decoded;  
+}
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return token;
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    const decoded = jwtDecode<JwtPayload>(token);
+    const currentTime = Date.now().valueOf() / 1000;
+    if (decoded.exp && decoded.exp < currentTime) {
+      return true;
+    }
+    
   }
 
   getToken(): string {
     // TODO: return the token
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser;
+  
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
+    localStorage.setItem('id_token', idToken);
     // TODO: redirect to the home page
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
+    localStorage.removeItem('id_token');
     // TODO: redirect to the login page
+    window.location.assign('/');
   }
 }
 
--- Develop/server/src/middleware/auth.ts
+++ Develop/server/src/middleware/auth.ts
@@ -1,10 +1,19 @@
-import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-
-interface JwtPayload {
-  username: string;
-}
-
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  // TODO: verify the token exists and add the user data to the request object
+export const authenticateToken = (req, res, next) => {
+    // TODO: verify the token exists and add the user data to the request object
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+        const token = authHeader.split("")[1];
+        const secretKey = process.env.JWT_SECRET_KEY || '';
+        jwt.verify(token, secretKey, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            req.user = user;
+            return next();
+        });
+    }
+    else {
+        res.sendStatus(401);
+    }
 };
